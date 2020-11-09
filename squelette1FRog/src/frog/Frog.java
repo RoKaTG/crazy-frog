24b03665a57d5ce99ec2be12053c355565c186fc
Samuel <monsinjon.samuel@gmail.com>
2020-11-10 00:18:22 +0100

    Fin premier TP

--- squelette1FRog/src/frog/Frog.java
+++ squelette1FRog/src/frog/Frog.java
@@ -2,10 +2,33 @@ package frog;
 
 import gameCommons.Game;
 import gameCommons.IFrog;
+import util.Case;
+import util.Direction;
 
 public class Frog implements IFrog {
 	
 	private Game game;
+	private Direction direction;
+	private Case position;
 
+	public Frog(Game game){
+		this.game = game;
+		this.direction = Direction.up;
+		this.position = new Case(0, game.width/2);
+	}
 
+	@Override
+	public Case getPosition() {
+		return this.position;
+	}
+
+	@Override
+	public Direction getDirection() {
+		return this.direction;
+	}
+
+	@Override
+	public void move(Direction key) {
+
+	}
 }
